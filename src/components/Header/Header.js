b4fa140f7a09486cbfde86a0182b8e1c285fa142
Cody Burley <codyburley@gmail.com>
2022-11-30 15:13:11 -0500

    finished header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMoon as solidMoon } from "@fortawesome/free-solid-svg-icons";
+import { faMoon as regularMoon } from "@fortawesome/free-regular-svg-icons";
 import './Header.scss';
 
-const Header = () => {
+const Header = ({ switchTheme }) => {
+  const [isDarkMode, setIsDarkMode] = useState(false);
   return (
-    <div>Header</div>
+    <header className='header'>
+      <Link to='/' className='header__link'>
+        <h1 className='header__title'>Where in the world?</h1>
+      </Link>
+      <div className="header__theme-container">
+        <FontAwesomeIcon
+          icon={!isDarkMode ? solidMoon : regularMoon}
+          className='header__theme-icon'
+        />
+        <button
+          className="header__theme-button"
+          onClick={() => { setIsDarkMode(!isDarkMode); switchTheme() }}>
+          Dark Mode
+        </button>
+      </div>
+
+    </header>
   )
 }
 
